fe578f3117d95d5e282d09eb529a7014e4fd5d01
thant htoo san <102045904+thant-san@users.noreply.github.com>
2022-04-27 23:07:35 +0630

    Create myapp.py

---
+++ myapp.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.title("Brain tumer classification")
+st.header("insert ur file below")
+uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=False)
+for uploaded_file in uploaded_files:
+     bytes_data = uploaded_file.read()
+     st.write("filename:", uploaded_file.name)
+     st.write(bytes_data)
