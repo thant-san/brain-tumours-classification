be0776a4a99c2e525b386d2e0150e30088f49a2f
thant htoo san <102045904+thant-san@users.noreply.github.com>
2022-04-28 20:13:19 +0630

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,9 +1,23 @@
 import streamlit as st
+from PIL import Image.ImageOps
+import matploblib.pyplot as plt
+import tensorflow as tf
+import tensorflow_hub as hub
+import numpy as np
+from tensorflow.keras import preprocessing
+from tensorflow.keras.model import load_model
+from tensorflow.keras.activation import softmax
+import os
 st.title("Brain tumer classification")
-st.header("insert ur file below")
-uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=False)
-for uploaded_file in uploaded_files:
-     bytes_data = uploaded_file.read()
-     st.write("filename:", uploaded_file.name)
-     st.write(bytes_data)
-model=load_model('cnn.h5')
+st.header("insert ur  mri file below")
+def main():
+     file_upload=st.file_uploader("choose the mri file",type=['jpg','png','jpeg'])
+     image=Image.open(file_upload)
+     figure=plt.figure()
+     plt.imshow(image)
+     plt.axis('off')
+     result=predict_class(image)
+     st.write(result)
+     st.pyplot(figure)
+def predict_class(image):
+     classifier_model=tf.keras.models.load_model(
