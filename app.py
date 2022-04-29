2420dc404512a75b1a86bfb31f4d44086674a97e
thant htoo san <102045904+thant-san@users.noreply.github.com>
2022-04-29 21:49:00 +0630

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ import streamlit as st
 st.title("Brain tumer classification")
 st.header("insert ur  mri file below")
 
-     file_upload=st.file_uploader("choose the mri file",type=['jpg','png','jpeg'])
-     image=Image.open(file_upload)
+file_upload=st.file_uploader("choose the mri file",type=['jpg','png','jpeg'])
+image=Image.open(file_upload)
      
