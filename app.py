1c1e2abde589e8f5a812495ee156721ab4c060e1
thant htoo san <102045904+thant-san@users.noreply.github.com>
2022-05-02 20:33:50 +0630

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-
-st.title("Brain tumer classification")
-st.header("insert ur  mri file below")
-
-file_upload=st.file_uploader("choose the mri file",type=['jpg','png','jpeg'])
-image=Image.open(file_upload)
-     
