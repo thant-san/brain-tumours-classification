c846593bf5bc9554dfa4c9197aa4a301ec1ec2e0
thant htoo san <102045904+thant-san@users.noreply.github.com>
2022-05-05 21:50:05 +0630

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -26,13 +26,13 @@ st.write("you have",string)
 option=st.selectbox('Select your tumor_type',('none','glioma_tumor','meningioma_tumor','pituitary_tumor'))
 st.write('You have selected',option)
 #if option=='glioma_tumor':
-    st.write('''Common signs and symptoms of gliomas include:
-               Headache.
+    st.write("""Common signs and symptoms of gliomas include:
+                Headache.
                 Nausea or vomiting.
                 Confusion or a decline in brain function.
                  Memory loss.
                 Personality changes or irritability.
                 Difficulty with balance.
                 Urinary incontinence.
-                Vision problems, such as blurred vision, double vision or loss of peripheral vision.''')
+                Vision problems, such as blurred vision, double vision or loss of peripheral vision""")
 else st.write('please'):
