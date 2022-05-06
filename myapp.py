eb6c3c4ef383256d8a9031b59c31658fb3d87e92
thant htoo san <102045904+thant-san@users.noreply.github.com>
2022-05-06 20:52:51 +0630

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -5,6 +5,13 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 
+from imp import load_compiled
+
+import json
+import requests
+from streamlit_lottie import st_lottie
+
+
 model=load_model('cnn_model_65eps.h5')
 
 st.title("Brain tumer classification",)
@@ -12,17 +19,24 @@ st.header("insert ur  mri image",)
 
 st.set_option('deprecation.showfileUploaderEncoding', False)
 file_upload=st.file_uploader("choose the mri file",type=['jpg','png','jpeg'])
-
-image = Image.open(file_upload)
-size=(227,227)
-image=ImageOps.fit(image,size,Image.ANTIALIAS)
-img=np.asarray(image)
-img_reshape=img[np.newaxis,...]
-st.image(img_reshape, caption='your mri image')
-prediction=model.predict(img_reshape)
-class_names=['glioma_tumor','meningioma_tumor','no_tumor','pituitary_tumor']
-string=class_names[np.argmax(prediction)]
-st.write("you have",string)
+if file_upload is None:
+    def load_lottieurl(url:str):
+    r=requests.get(url)
+    if r.status_code !=200:
+        return None
+    return r.json()
+ else lottie_coding=load_lottieurl("https://assets7.lottiefiles.com/packages/lf20_iarc855d.json"):
+    st_lottie(lottie_coding,height=100,width=100,key=None)
+    image = Image.open(file_upload)
+    size=(150,150)
+    image=ImageOps.fit(image,size,Image.ANTIALIAS)
+    img=np.asarray(image)
+    img_reshape=img[np.newaxis,...]
+    st.image(img_reshape, caption='your mri image')
+    prediction=model.predict(img_reshape)
+    class_names=['glioma','meningioma','notumor','pituitary']
+    string=class_names[np.argmax(prediction)]
+    st.write("you have",string)
 
 import streamlit as st
 expander=st.expander(" about symptons of tumor")
