fad59b60eb4b0d51a1356fd47088295c073424d7
thant htoo san <102045904+thant-san@users.noreply.github.com>
2022-05-07 00:06:37 +0630

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -78,6 +78,6 @@ with expander:
          st.write('please select')
 st.sidebar.write('Developed by INTEL AI')
 lottie_contact=load_lottieurl("https://assets7.lottiefiles.com/packages/lf20_zj3qnsfs.json")
-ani=st_lottie(lottie_contact,height=60,width=60,key=None)
+st_lottie(lottie_contact,height=60,width=60,key=None)
 st.sidebar.write("""If you have any issues contact us
-                 thanthtoosan.mechatronic@gmail.com""",'ani')
+                 thanthtoosan.mechatronic@gmail.com""",st_lottie(lottie_contact,height=60,width=60,key=None))
